3fdedceac0782d0783201b2168fd7b65aa6227e8
CODA29 <rasdagm@gmail.com>
2025-01-26 20:32:03 -0800

    Feat: added week-2 folder and make changes to week-3 project

--- week-3/project02-04/project02-04_txt.js
+++ week-3/project02-04/project02-04_txt.js
@@ -4,32 +4,32 @@
 
       Application to calculate the cost of a restaurant order plus tax
       Author: Dagmawi Megra
-      Date: 01/25/2025 
+      Date: 01/25/2025
 
       Filename: project02-04.js
  */
- chicken_price = 10.95;
- halibut_price = 13.95;
- burger_price = 9.95;
- salmon_price = 18.95;
- salad_price = 7.95;
- sales_tax = 0.07;
+ const chicken_price = 10.95;
+ const halibut_price = 13.95;
+ const burger_price = 9.95;
+ const salmon_price = 18.95;
+ const salad_price = 7.95;
+ const sales_tax = 0.07;
 
  document.getElementById("chicken").addEventListener("click", calcTotal);
  document.getElementById("halibut").addEventListener("click", calcTotal);
  document.getElementById("burger").addEventListener("click", calcTotal);
  document.getElementById("salmon").addEventListener("click", calcTotal);
  document.getElementById("salad").addEventListener("click", calcTotal);
- 
+
  function calcTotal(){
-   
-   cost = 0;
-   
-   const buyChicken = document.getElementById("chicken").checked;
-   const buyHalibut = document.getElementById("halibut").checked;
-   const buyBurger = document.getElementById("burger").checked;
-   const buySalmon = document.getElementById("salmon").checked;
-   const buySalad = document.getElementById("salad").checked;
+
+   let cost = 0;
+
+   let buyChicken = document.getElementById("chicken").checked;
+   let buyHalibut = document.getElementById("halibut").checked;
+   let buyBurger = document.getElementById("burger").checked;
+   let buySalmon = document.getElementById("salmon").checked;
+   let buySalad = document.getElementById("salad").checked;
 
    cost += buyChicken ? chicken_price : 0;
    cost += buyHalibut ? halibut_price : 0;
@@ -50,7 +50,7 @@
  }
 
 
-// Function to display a numeric value as a text string in the format $##.## 
+// Function to display a numeric value as a text string in the format $##.##
  function formatCurrency(value) {
     return "$" + value.toFixed(2);
  }
